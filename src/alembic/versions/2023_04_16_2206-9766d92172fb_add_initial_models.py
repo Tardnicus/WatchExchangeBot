c8791b0bbde189b338c4afed956d223474713044
Nectarios Chroniaris <nchroniaris@gmail.com>
2023-04-16 22:24:22 -0600

    Add initial models

---
+++ src/alembic/versions/2023_04_16_2206-9766d92172fb_add_initial_models.py
@@ -0,0 +1,55 @@
+"""Add initial models
+
+Revision ID: 9766d92172fb
+Revises:
+Create Date: 2023-04-16 22:06:38.727112
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "9766d92172fb"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "processed_post",
+        sa.Column("id", sa.String(), autoincrement=False, nullable=False),
+        sa.Column("date_processed", sa.DateTime(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+    op.create_table(
+        "submission_criterion",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column(
+            "submission_type",
+            sa.Enum("WTB", "WTS", name="submissiontype"),
+            nullable=False,
+        ),
+        sa.Column("min_transactions", sa.Integer(), nullable=False),
+        sa.Column("all_required", sa.Boolean(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+    op.create_table(
+        "keyword",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("content", sa.String(), nullable=False),
+        sa.Column("criterion_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["criterion_id"],
+            ["submission_criterion.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("keyword")
+    op.drop_table("submission_criterion")
+    op.drop_table("processed_post")
---
+++ src/wemb/models.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from enum import Enum, unique
+from typing import List
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+@unique
+class SubmissionType(Enum):
+    """Represents a type of submission (either WTS or WTB)."""
+
+    def __init__(self, value):
+        # Formats a string such as [wts] or [wtb] for comparison purposes
+        self.formatted_value = f"[{str(value).lower()}]"
+
+    WTB = "WTB"
+    WTS = "WTS"
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Keyword(Base):
+    """Represents a single keyword used as part of a SubmissionCriterion."""
+
+    __tablename__ = "keyword"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    content: Mapped[str] = mapped_column()
+
+    criterion_id: Mapped[int] = mapped_column(ForeignKey("submission_criterion.id"))
+    criterion: Mapped["SubmissionCriterion"] = relationship(back_populates="keywords")
+
+
+class SubmissionCriterion(Base):
+    """Represents a single criterion to be matched against posts."""
+
+    __tablename__ = "submission_criterion"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    submission_type: Mapped[SubmissionType]
+    min_transactions: Mapped[int] = mapped_column(default=5)
+    keywords: Mapped[List["Keyword"]] = relationship(
+        back_populates="criterion", cascade="all, delete, delete-orphan"
+    )
+    all_required: Mapped[bool] = mapped_column(default=True)
+
+
+class ProcessedPost(Base):
+    """Used as a cache to keep track of already-processed posts, to prevent a restart from triggering another notification."""
+
+    __tablename__ = "processed_post"
+
+    id: Mapped[str] = mapped_column(primary_key=True, autoincrement=False)
+    date_processed: Mapped[datetime] = mapped_column(default=datetime.now)
